0a47466702c3e2d984ece5576a31ab74a48952aa
Ujwal Nandanwar <75965943+un0038998@users.noreply.github.com>
2021-02-05 23:48:08 -0500

    Update Relay.ino

--- Relay/Relay.ino
+++ Relay/Relay.ino
@@ -4,7 +4,8 @@ void setup()
   pinMode(RELAY_PIN, OUTPUT);
 }
 
-//Switch on/off relay after every 5 seconds
+//Switch on/off relay after every 5 seconds. This is low level triggered relay.
+//So sending LOW signal will turn on relay and HIGH signal will turn off relay.
 void loop() 
 {
   digitalWrite(RELAY_PIN, LOW);
