2094f763a1df92907cf921ea81f57f6ba441db85
Ujwal Nandanwar <75965943+un0038998@users.noreply.github.com>
2021-04-21 00:36:25 -0400

    Add files via upload

--- Relay/Relay.ino
+++ Relay/Relay.ino
@@ -1,15 +1,16 @@
-#define RELAY_PIN 3
+#define RELAY_PIN 2
+
 void setup() 
 {
   pinMode(RELAY_PIN, OUTPUT);
 }
 
-//Switch on/off relay after every 5 seconds. This is low level triggered relay.
+//Switch on/off relay after every 2 seconds. This is low level triggered relay.
 //So sending LOW signal will turn on relay and HIGH signal will turn off relay.
 void loop() 
 {
   digitalWrite(RELAY_PIN, LOW);
-  delay(5000);
+  delay(2000);
   digitalWrite(RELAY_PIN, HIGH);
-  delay(5000);  
+  delay(2000);  
 }
