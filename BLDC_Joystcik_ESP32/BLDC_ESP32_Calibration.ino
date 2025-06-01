a89ceadb76c8456fe31e2ea71429f04ef8faa1e4
Ujwal Nandanwar <75965943+un0038998@users.noreply.github.com>
2025-06-01 18:01:55 +0530

    Add files via upload

---
+++ BLDC_Joystcik_ESP32/BLDC_ESP32_Calibration.ino
@@ -0,0 +1,19 @@
+#include <ESP32Servo.h>
+
+#define ESC_PIN 16
+
+Servo esc;
+
+void setup() 
+{
+  esc.attach(ESC_PIN,  1000, 2000);
+  esc.write(180);
+  delay(5000);
+  esc.write(0);
+  delay(2000);
+  esc.write(10);  
+}
+
+void loop() 
+{
+}
